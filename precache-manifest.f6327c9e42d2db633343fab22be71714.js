0ef8c47aaace1a4080ee3ccb7ab0224309abda77
yadi-developer <triyadimulyana1@gmail.com>
2021-09-21 16:43:47 +0700

    first commit

---
+++ precache-manifest.f6327c9e42d2db633343fab22be71714.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f5fbaa5e9f80bf76a31",
+    "url": "/css/app.4b4cdcba.css"
+  },
+  {
+    "revision": "c93bf64b2a7598a67575baf1ee478a6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ecd466154d5e9c62b83",
+    "url": "/js/about.51e6030e.js"
+  },
+  {
+    "revision": "8f5fbaa5e9f80bf76a31",
+    "url": "/js/app.4afddbd4.js"
+  },
+  {
+    "revision": "2b2554c9628b858978da",
+    "url": "/js/chunk-vendors.2338e579.js"
+  },
+  {
+    "revision": "46c2c366138bafeb12b48ea20d145bc6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
